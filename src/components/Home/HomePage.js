e7e290f55c8c7fd0ee8280a65d962ecdf6de7809
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-03-28 17:51:43 -0300

    Frontend, intentando acceder a el value del formulario

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,27 +1,7 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
 
 export function HomePage() {
-  const [user, setUser] = useState([])
 
-  const fetchUsers = async () => {
-    const url = 'http://18.118.149.101:3000/red/user'
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-    })
-    const data = await response.json()
-    console.log(user);
-    setUser(data)
-  }
-
-  useEffect(() => {
-    fetchUsers()
-  }, [])
   return (
     <p>HomePage</p>
   )
--- src/components/SingUp/SingUp.js
+++ src/components/SingUp/SingUp.js
@@ -1,22 +1,62 @@
 import React from 'react';
+import { useState } from "react";
 import './SingUp.css'
 
 export function SingUp() {
+  const [user, setUser] = useState([{
+    user: '',
+    nameUser: '',
+    lastName: '',
+    email: '',
+    password: ''
+  }])
+
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = 'http://18.118.149.101:3000/red/user'
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    })
+    const data = await response.json()
+    if (data.length > 0) {
+      setError('El email ya ha sido registrado');
+      console.log(error);
+      console.log(data[0].user);
+    } else {
+      console.log(user);
+      console.log(data);
+      setUser(data)
+    }
+  }
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
 
   return (
     <div>
       <h1>SingUp</h1>
 
       <section>
-        <section className='registro'>
-          <input placeholder='Usuario' />
-          <input placeholder='Nombre' />
-          <input placeholder='Apellido' />
-          <input placeholder='Email' />
-          <input placeholder='password' />
+        <form onSubmit={handleSubmit} className='registro'>
+          <input
+            name='usuario'
+            placeholder='Usuario'
+            value={user.user}
+            onChange={handleChange}
+          />
+          <input placeholder='Nombre' type='text' />
+          <input placeholder='Apellido' type='text' />
+          <input placeholder='Email' type='email' />
+          <input placeholder='password' type='password' />
 
           <button type="submit" >Registrarse</button>
-        </section>
+        </form>
       </section>
     </div>
   );
