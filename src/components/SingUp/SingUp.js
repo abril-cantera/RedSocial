a86a18ee45b43c79a94f66423ee290df65faf272
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-03-30 17:41:27 -0300

    Funciona pero solo dando doble click

--- src/components/SingUp/SingUp.js
+++ src/components/SingUp/SingUp.js
@@ -8,8 +8,12 @@ export function SingUp() {
   const [user, setUser] = useState('')
   const [email, setEmail] = useState('')
 
-  const getData = async () => {
+  let newArrayUsers = []
+  let newArrayEmails = []
 
+  const exito = 'Registrado con exito'
+
+  const getDataa = async (data) => {
     const url = 'http://18.218.88.225:3000/red/user'
     const response = await fetch(url, {
       method: 'GET',
@@ -17,18 +21,42 @@ export function SingUp() {
         'Content-Type': 'application/json'
       }
     })
-    const data = await response.json()
+    data = await response.json()
     console.log(data);
-    setUsers(users)
-
-
+    //
   }
-  getData()
 
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   // setUsuario(event.target.usuario.value)
-  //   // setCorreo(event.target.correo.value)
+  const getData = async (event) => {
+    event.preventDefault();
+    setUser(event.target.usuario.value)
+    setEmail(event.target.correo.value)
+
+    const url = 'http://18.218.88.225:3000/red/user'
+    const response = await fetch(url)
+    const data = await response.json()
+    getDataa()
+
+    data.forEach((element) => {
+      const arrayElement = Object.values(element)
+      newArrayUsers.push(arrayElement[1])
+      newArrayEmails.push(arrayElement[2])
+    });
+
+    switch (true) {
+      case newArrayUsers.includes(user):
+        console.log('El usuario ya existe');
+      case newArrayEmails.includes(email):
+        console.log('El email ya existe');
+        break;
+      default:
+        console.log(exito);
+        break;
+    }
+    setUsers(users)
+  }
+
+
+  // const handleSubmit = async () => {
 
   //   const url = 'http://18.218.88.225:3000/red/user'
   //   const response = await fetch(url, {
@@ -44,40 +72,20 @@ export function SingUp() {
   //   const data = await response.json()
   //   console.log(data);
   //   setUsers(users)
-
   // }
-  const postData = async () => {
-    const url = 'http://18.218.88.225:3000/red/user'
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const data = await response.json()
-
-    data.map((item) => {
-      // const itemsArray = Object.values(item)
-      // console.log(itemsArray);
-      console.log(item);
-      console.log(user);
-      console.log(email);
-
-    })
-  }
 
   return (
     <div>
       <h1>SingUp</h1>
       <section>
-        <form onSubmit={postData} className='registro'>
+        <form onSubmit={getData} className='registro'>
           <label>
             User:
             <input
               name='usuario'
               placeholder='Usuario'
               type='text'
-              onChange={(e) => setUser(e.target.value)}
+            // onChange={(e) => setUser(e.target.value)}
             />
           </label>
           <label>
@@ -86,7 +94,7 @@ export function SingUp() {
               name='correo'
               placeholder='Email'
               type='email'
-              onChange={(e) => setEmail(e.target.value)}
+            // onChange={(e) => setEmail(e.target.value)}
             />
           </label>
           <button type="submit" >Registrarse</button>
@@ -97,10 +105,3 @@ export function SingUp() {
   );
 }
 
-// e => {
-// e.preventDefault()
-// setUsuario(e.target.usuario.value)
-// setCorreo(e.target.correo.value)
-// console.log(usuario);
-// console.log(correo);
-//         }
