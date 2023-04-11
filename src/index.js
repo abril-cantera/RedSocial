82dcf43005eac17709134935dda9961a5b105095
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-04-11 12:43:25 -0300

    ..

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const app = ReactDOM.createRoot(document.getElementById('app'));
+app.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
